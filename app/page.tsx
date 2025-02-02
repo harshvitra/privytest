7b98c055678ef3c8c753270b396f128f3c0a6ad2
Kartikey353 <shyamsharmamohan444@gmail.com>
2025-02-02 13:09:46 +0530

    adding contracts

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <LoginButton />
+        <LoginButton /> 
+
       </div>
     </PrivyProvider>
   );
